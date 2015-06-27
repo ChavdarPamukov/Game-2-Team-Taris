b5fc05d921941bdb4b03baf5330de47b39346ab5
Velichka Moralieva <vili.moralieva@gmail.com>
2015-06-27 17:15:11 +0300

    Added new Helpform

---
+++ Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/HelpForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Еscape_from_the_labyrinth_of_death
+{
+    public partial class HelpForm : Form
+    {
+        public HelpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CharacterForm charF = new CharacterForm();
+            charF.ShowDialog();
+        }
+    }
+}
