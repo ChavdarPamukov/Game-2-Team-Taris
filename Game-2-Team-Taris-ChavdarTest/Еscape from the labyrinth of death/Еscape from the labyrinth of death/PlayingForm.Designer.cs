282470c06f2d40940feae7c00c5cb31ce1bdc8d8
Miroslav Galabov <wolfnature13@gmail.com>
2015-06-30 00:59:29 +0300

    3
    3

--- Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+++ Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
@@ -67,8 +67,11 @@
             this.wall_2 = new System.Windows.Forms.PictureBox();
             this.wall_1 = new System.Windows.Forms.PictureBox();
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
             this.playerBox = new System.Windows.Forms.PictureBox();
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
@@ -130,8 +133,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.wall_2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wall_1)).BeginInit();
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
             ((System.ComponentModel.ISupportInitialize)(this.playerBox)).BeginInit();
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
@@ -182,8 +188,11 @@
             this.playingPn.Controls.Add(this.wall_2);
             this.playingPn.Controls.Add(this.wall_1);
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
             this.playingPn.Controls.Add(this.playerBox);
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
@@ -557,6 +566,7 @@
             this.wall_1.TabStop = false;
             // 
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
             // playerBox
             // 
@@ -569,6 +579,8 @@
             this.playerBox.TabStop = false;
             this.playerBox.Click += new System.EventHandler(this.player_Click);
             // 
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
@@ -843,8 +855,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.wall_2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.wall_1)).EndInit();
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
             ((System.ComponentModel.ISupportInitialize)(this.playerBox)).EndInit();
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
@@ -865,8 +880,11 @@
         private System.Windows.Forms.Label NaPlayer;
         private System.Windows.Forms.Label CharPlayer;
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
         private System.Windows.Forms.PictureBox playerBox;
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.Designer.cs
 =======
--- Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+++ Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
@@ -183,12 +183,15 @@ namespace Еscape_from_the_labyrinth_of_death
         }
 
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
 <<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
         private void player_Click(object sender, EventArgs e)
         {
 
         }
 =======
+=======
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
         private void labelCurrentlyEquiped_Click(object sender, EventArgs e)
@@ -208,6 +211,9 @@ namespace Еscape_from_the_labyrinth_of_death
             helpF.ShowDialog();
         }
 <<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+<<<<<<< HEAD:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+=======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
 =======
 >>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
