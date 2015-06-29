5f3e76837897c8f1c4175eecf7ce44ff56e08f40
Miroslav Galabov <wolfnature13@gmail.com>
2015-06-30 00:55:34 +0300

    premerge
    premerge

---
+++ Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Еscape_from_the_labyrinth_of_death
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.CreditsButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.HButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreditsButton
+            // 
+            this.CreditsButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.CreditsButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreditsButton.Location = new System.Drawing.Point(55, 336);
+            this.CreditsButton.Name = "CreditsButton";
+            this.CreditsButton.Size = new System.Drawing.Size(90, 50);
+            this.CreditsButton.TabIndex = 1;
+            this.CreditsButton.Text = "CREDITS";
+            this.CreditsButton.UseVisualStyleBackColor = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.StartButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartButton.Location = new System.Drawing.Point(229, 336);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(90, 50);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // HButton
+            // 
+            this.HButton.BackColor = System.Drawing.Color.Yellow;
+            this.HButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HButton.Location = new System.Drawing.Point(407, 336);
+            this.HButton.Name = "HButton";
+            this.HButton.Size = new System.Drawing.Size(90, 50);
+            this.HButton.TabIndex = 3;
+            this.HButton.Text = "HELP";
+            this.HButton.UseVisualStyleBackColor = false;
+            this.HButton.Click += new System.EventHandler(this.HButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 32.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(550, 52);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Еscape from the labyrinth of death";
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Еscape_from_the_labyrinth_of_death.Properties.Resources.stones_wall;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(556, 437);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.CreditsButton);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Еscape from the labyrinth of death";
+            this.Load += new System.EventHandler(this.StartForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreditsButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button HButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Еscape_from_the_labyrinth_of_death.Interfaces;
+using Еscape_from_the_labyrinth_of_death.Factories;
+using Еscape_from_the_labyrinth_of_death.Classes.EnumClasses;
+
+namespace Еscape_from_the_labyrinth_of_death
+{
+    public partial class PlayingForm : Form
+    {
+        private List<PictureBox> _wallsList;
+        private IPlayerFactory _factory = PlayerFactory.PLAYER_FACTORY;
+        private IHumanPlayer _humanPlayer;
+
+        public PlayingForm(string UserName, string CharName)
+        {
+            InitializeComponent();
+            UpdateWallsList();
+            NaPlayer.Text = UserName;
+            CharPlayer.Text = CharName;
+            this._humanPlayer = (IHumanPlayer)this._factory.Create(this.playerBox, this._wallsList, PlayerClass.Elf);
+        }
+
+        private void PlayingForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayingForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Rectangle rect = playerBox.Bounds;
+
+            if (e.KeyCode == Keys.Left)
+            {
+                this._humanPlayer.MoveLeft();
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                this._humanPlayer.MoveRight();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                this._humanPlayer.MoveUp();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                this._humanPlayer.MoveDown();
+            }
+        }
+
+        private void MoveLeft(Rectangle rect)
+        {
+            rect.X -= 5;
+            if (HasColision(rect))
+            {
+                return;
+            }
+            this.playerBox.Bounds = rect;
+        }
+
+        private void MoveRight(Rectangle rect)
+        {
+            rect.X += 5;
+            if (HasColision(rect))
+            {
+                return;
+            }
+            this.playerBox.Bounds = rect;
+        }
+
+        private void MoveDown(Rectangle rect)
+        {
+            rect.Y += 5;
+            if (HasColision(rect))
+            {
+                return;
+            }
+            this.playerBox.Bounds = rect;
+        }
+
+        private void MoveUp(Rectangle rect)
+        {
+            rect.Y -= 5;
+            if (HasColision(rect))
+            {
+                return;
+            }
+            this.playerBox.Bounds = rect;
+        }
+
+        private bool HasColision(Rectangle rect)
+        {
+            foreach (PictureBox wall in this._wallsList)
+            {
+                if (rect.IntersectsWith(wall.Bounds))
+                {
+                    return true;
+                }
+            }
+            this.playerBox.Bounds = rect;
+
+            return false;
+        }
+
+        private void PlayingForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void playingPn_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbDefense_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbAttack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void UpdateWallsList()
+        {
+            this._wallsList = new List<PictureBox>()
+            {
+                wall_1,
+                wall_2,
+                wall_3,
+                wall_4,
+                wall_5,
+                wall_6,
+                wall_7,
+                wall_8,
+                wall_9,
+                wall_10,
+                wall_11,
+                wall_12,
+                wall_13,
+                wall_14,
+                wall_15,
+                wall_16,
+                wall_17,
+                wall_18,
+                wall_19,
+                wall_20,
+                wall_21,
+                wall_22,
+                wall_23,
+                wall_24,
+                wall_25,
+                wall_26,
+                wall_27,
+                wall_28,
+                wall_29,
+                wall_30,
+                wall_31,
+                wall_32,
+                wall_33,
+            };
+        }
+
+<<<<<<< HEAD:Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+        private void player_Click(object sender, EventArgs e)
+        {
+
+        }
+=======
+        private void labelCurrentlyEquiped_Click(object sender, EventArgs e)
+        {
+            Currently_Equiped currentlyEquiped = new Currently_Equiped();
+            currentlyEquiped.ShowDialog();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            HelpForm helpF = new HelpForm();
+            helpF.ShowDialog();
+        }
+>>>>>>> origin/ChavdarTest:Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/PlayingForm.cs
+    }
+}
