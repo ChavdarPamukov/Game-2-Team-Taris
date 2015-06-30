591a5b139a0ec6f58b372c013df0f5391f31653b
Velichka Moralieva <vili.moralieva@gmail.com>
2015-06-30 11:03:47 +0300

    New forms- winner and Loser

---
+++ Game-2-Team-Taris-ChavdarTest/Еscape from the labyrinth of death/Еscape from the labyrinth of death/Winner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Еscape_from_the_labyrinth_of_death
+{
+    public partial class Winner : Form
+    {
+        public Winner()
+        {
+            InitializeComponent();
+        }
+
+        private void Winner_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
